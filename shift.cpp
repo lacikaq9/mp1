639f95435cb701e3406ddaa917a351a8a4174775
lacikaq9 <43678530+lacikaq9@users.noreply.github.com>
2018-09-28 18:07:31 +0200

    Add files via upload

---
+++ shift.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main () {
+	unsigned int a = 1, count = 0;
+
+	while(a != 0)
+	{
+		a <<= 1;
+		cout << a << endl;
+		count ++;
+	}
+
+	cout << "\ " << count << " lépés után lesz 0.\n";
+
+	return 0;
+}
